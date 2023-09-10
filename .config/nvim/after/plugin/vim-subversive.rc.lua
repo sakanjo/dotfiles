3c443822088da6c77a867e26580ea02b4e67b5f8
sakanjo <dev.salah.kanjo@gmail.com>
2023-09-10 15:57:12 +0300

    feat: updated vim-subversive.rc.lua

--- .config/nvim/after/plugin/vim-subversive.rc.lua
+++ .config/nvim/after/plugin/vim-subversive.rc.lua
@@ -5,3 +5,7 @@ end
 vim.keymap.set('n', 'gr', '<Plug>(SubversiveSubstitute)', { silent = true })
 vim.keymap.set('n', 'grr', '<plug>(SubversiveSubstituteLine)', { silent = true })
 vim.keymap.set('n', 'gR', '<plug>(SubversiveSubstituteToEndOfLine)', { silent = true })
+
+vim.keymap.set('n', '<leader>gr', '<plug>(SubversiveSubstituteRange)', { silent = true })
+vim.keymap.set('x', '<leader>gr', '<plug>(SubversiveSubstituteRange)', { silent = true })
+vim.keymap.set('n', '<leader>grr', '<plug>(SubversiveSubstituteWordRange)', { silent = true })
