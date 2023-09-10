56c85a9e1352ac0a3e1c700a4bac10832392bf54
sakanjo <dev.salah.kanjo@gmail.com>
2023-09-10 15:20:07 +0300

    chore: changed keymap

--- .config/nvim/after/plugin/vim-subversive.rc.lua
+++ .config/nvim/after/plugin/vim-subversive.rc.lua
@@ -2,6 +2,6 @@ if not vim.fn.exists('g:loadedSubversive') then
   return
 end
 
-vim.keymap.set('n', '<leader>r', '<Plug>(SubversiveSubstitute)', { silent = true })
-vim.keymap.set('n', '<leader>rr', '<plug>(SubversiveSubstituteLine)', { silent = true })
-vim.keymap.set('n', '<leader>R', '<plug>(SubversiveSubstituteToEndOfLine)', { silent = true })
+vim.keymap.set('n', 'gr', '<Plug>(SubversiveSubstitute)', { silent = true })
+vim.keymap.set('n', 'grr', '<plug>(SubversiveSubstituteLine)', { silent = true })
+vim.keymap.set('n', 'gR', '<plug>(SubversiveSubstituteToEndOfLine)', { silent = true })
