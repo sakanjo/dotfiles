ea05983ccfd0bb18c71fcec79750e71a69594552
sakanjo <dev.salah.kanjo@gmail.com>
2023-09-13 17:25:16 +0300

    feat: update keymap

--- .config/nvim/after/plugin/vim-textobj-between.rc.lua
+++ .config/nvim/after/plugin/vim-textobj-between.rc.lua
@@ -1,6 +1,6 @@
 vim.g.textobj_between_no_default_key_mappings = true
 
-vim.keymap.set('o', 'aq', '<Plug>(textobj-between-a)')
-vim.keymap.set('x', 'aq', '<Plug>(textobj-between-a)')
-vim.keymap.set('o', 'iq', '<Plug>(textobj-between-i)')
-vim.keymap.set('x', 'iq', '<Plug>(textobj-between-i)')
+vim.keymap.set('o', 'aQ', '<Plug>(textobj-between-a)')
+vim.keymap.set('x', 'aQ', '<Plug>(textobj-between-a)')
+vim.keymap.set('o', 'iQ', '<Plug>(textobj-between-i)')
+vim.keymap.set('x', 'iQ', '<Plug>(textobj-between-i)')
