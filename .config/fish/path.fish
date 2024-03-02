ff85696b24a066139702289e9db093ecfe0b5f53
Salah Kanjo <dev.salah.kanjo@gmail.com>
2024-03-02 22:20:53 +0300

    chore: updated path.fish

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -1,3 +1,4 @@
+# Local
 set -gx PATH ~/.local/bin $PATH
 
 # NodeJs
@@ -7,8 +8,23 @@ set -gx PATH node_modules/.bin $PATH
 set -gx PATH vendor/bin $PATH
 set -gx PATH ~/.config/composer/vendor/bin $PATH
 
+# PNPM
+set -gx PNPM_HOME ~/.local/share/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+
+# Cargo
+set -gx PATH ~/.cargo/bin $PATH
+
+# Flutter
+set -gx PATH ~/.local/flutter/bin $PATH
+
+# Android
+set -gx PATH ~/Android/Sdk/emulator $PATH
+
 # Go
-set -g GOPATH ~/go
+set -gx GOPATH ~/go
 set -gx PATH $GOPATH/bin $PATH
 
 # Bun
